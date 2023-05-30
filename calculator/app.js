b69bf6338e89b79aa03874bb1276b76ed0e7de32
kayalvizhikamalakannan <sarakayalvizhi@gmail.com>
2023-05-30 17:20:21 +0530

    add

---
+++ calculator/app.js
@@ -0,0 +1,13 @@
+const addBtn = document.getElementById('btn-add');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add() {
+    currentResult = currentResult + parseInt(userInput.value);
+    //currentResult = currentResult + +userInput.value;
+    outputResult(currentResult, '');
+}
+
+addBtn.addEventListener('click', add);
+
+ 
