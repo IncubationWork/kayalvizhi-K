f5ec67fd7b63352c3d99df5c7f30053579055460
kayalvizhikamalakannan <sarakayalvizhi@gmail.com>
2023-05-31 19:05:11 +0530

    screenshot

--- calculator/app.js
+++ calculator/app.js
@@ -1,13 +1,61 @@
-const addBtn = document.getElementById('btn-add');
+
 const defaultResult = 0;
 let currentResult = defaultResult;
+//let logEntries;
+let logEntries = [];
+
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+/*function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+} 
+
+function add(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}*/
 
 function add() {
-    currentResult = currentResult + parseInt(userInput.value);
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult += enteredNumber;
     //currentResult = currentResult + +userInput.value;
-    outputResult(currentResult, '');
+    outputResult(currentResult, calcDescription);
+    //logEntries = [enteredNumber];
+    //logEntries.push(enteredNumber);
+    //console.log(logEntries)
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} - ${enteredNumber}`;
+    currentResult -= enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} * ${enteredNumber}`;
+    currentResult *= enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} / ${enteredNumber}`;
+    currentResult /= enteredNumber;
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
 
- 
+subtractBtn.addEventListener('click', subtract);
+
+multiplyBtn.addEventListener('click', multiply);
+
+divideBtn.addEventListener('click', divide);
