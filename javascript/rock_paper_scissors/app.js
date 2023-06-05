21137e26662a5b8c3adf64fc06971f15c04edcb7
kayalvizhikamalakannan <sarakayalvizhi@gmail.com>
2023-06-05 18:57:29 +0530

    Rock,Paper or scissors

---
+++ javascript/rock_paper_scissors/app.js
@@ -0,0 +1,69 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function(){
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}`, '').toUpperCase();
+    if(selection !== ROCK && selection !== PAPER && selection !== SCISSORS){
+        alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for You!`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+};
+
+const getComputerChoice = function(){
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+};
+
+const getWinner = function(cChoice, pChoice = DEFAULT_USER_CHOICE) {
+    if (cChoice === pChoice) {
+        return RESULT_DRAW;
+    } else if (cChoice === ROCK && pChoice === PAPER ||
+        cChoice === PAPER && pChoice === SCISSORS ||
+        cChoice === SCISSORS && pChoice === ROCK){
+            return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
+};
+
+startGameBtn.addEventListener('click', function() {
+    if(gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true;
+    console.log(gameIsRunning);
+    console.log('Game is starting...');
+    const playerSelection = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+    let winner;
+    if (playerSelection) {
+        winner = getWinner(computerChoice, playerSelection);
+    } else {
+        winner = getWinner(computerChoice);
+    }
+    console.log(computerChoice);
+    alert(`Your Choice is ${playerSelection} and the Computer Choice is ${computerChoice}. The Result is: ${winner}`);
+    gameIsRunning = false;
+});
+
+//function startGame() {
+//    console.log('Game is starting....');
+//}
+//
+//startGameBtn.addEventListener('click', startGame);
